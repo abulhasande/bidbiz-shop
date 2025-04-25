bbc55cb4193d4d690bc198316be361a66e4b141e
gbeydtha <beydtha@gmail.com>
2025-04-25 05:01:32 +0200

    Add project files.

---
+++ Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,35 @@
+﻿namespace Catalog.Api.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+         : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler(IDocumentSession session)
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Create Product Entity from Command Obj
+            var product = new Product()
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+
+            //Save to DB
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //Return CreateProductResult result
+
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
