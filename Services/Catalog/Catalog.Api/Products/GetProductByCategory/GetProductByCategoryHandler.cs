920a29b9e38a27a07da74b4fe35a5caf4b769828
gbeydtha <beydtha@gmail.com>
2025-04-25 20:35:40 +0200

    Add edit delete and bycategory filter

---
+++ Services/Catalog/Catalog.Api/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,26 @@
+﻿
+using Catalog.Api.Products.GetProductById;
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.Api.Products.GetProductByCategory
+{
+
+    public record GetProductByCategoryQuery(string category): IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+
+    public class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        : IRequestHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async  Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+            var products = await session.Query<Product>()
+                                        .Where(p => p.Category.Contains(query.category))
+                                        .ToListAsync();
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
