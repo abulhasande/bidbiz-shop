ab54fdae9551596b192f814b4f9c6d10ca371893
gbeydtha <beydtha@gmail.com>
2025-04-30 17:40:18 +0200

    Create Ordering API, Infras, Application and DOmain

---
+++ Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿namespace Ordering.Api
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+
+            //services.AddCarter()
+
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            //services.MapCarter()
+
+            return app;
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Api/Program.cs
@@ -0,0 +1,18 @@
+using Ordering.Api;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add Services to the container.
+
+builder.Services
+       .AddApplicationServices()
+       .AddInfrastructureServices(builder.Configuration)
+       .AddApiServices();
+
+var app = builder.Build();
+
+//Configure the HTTP Pipeline.
+
+app.Run();
