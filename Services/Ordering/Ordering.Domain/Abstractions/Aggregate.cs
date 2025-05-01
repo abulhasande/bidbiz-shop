4fd1e6400ed946ac0c128e694e172b6f0546ddb2
gbeydtha <beydtha@gmail.com>
2025-05-01 02:03:18 +0200

    Implement Domain using DDD Architechure for Order domain

---
+++ Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvent = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _domainEvent.AsReadOnly();
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _domainEvent.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDoaminEvents()
+        {
+            IDomainEvent[] dequeueEventss = _domainEvent.ToArray();
+
+            _domainEvent.Clear();
+
+            return dequeueEventss;
+        }
+    }
+}
---
+++ Services/Ordering/Ordering.Domain/Events/OrderCreateEvent.cs
@@ -0,0 +1,8 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Models;
+
+namespace Ordering.Domain.Events
+{
+    public record OrderCreateEvent(Order order) : IDomainEvent;
+
+}
